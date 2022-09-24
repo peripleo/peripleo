a75ca4096f456070b0d4ca3388144aff7f3f3418
Rainer Simon <rainer@rainersimon.io>
2022-09-24 21:13:25 +0200

    Upgraded deps to latest versions

--- src/state/index.js
+++ src/state/index.js
@@ -1,8 +1,17 @@
 import { atom } from 'recoil';
-
 import { initialView } from './URLState';
 
 export const mapViewState = atom({
   key: 'mapView',
   default: initialView
+});
+
+export const mapHoverState = atom({
+  key: 'mapHover',
+  default: null
+});
+
+export const selectionState = atom({
+  key: 'selection',
+  default: null
 });
