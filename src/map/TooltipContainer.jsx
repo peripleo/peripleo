830f4934a9b7330fd2d1acf78f60462eff30844f
Rainer Simon <rainer@rainersimon.io>
2022-09-25 19:08:59 +0200

    Tooltip container now stays insde map bounds

--- src/map/TooltipContainer.jsx
+++ src/map/TooltipContainer.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 // TODO make this dynamic!
 const OFFSET = [15, 15];
 
 const TooltipContainer = props => {
 
+  const el = useRef();
+
   const {x, y, node, feature, tooltip} = props;
   
-  const left = x + OFFSET[0];
-  const top = y + OFFSET[1];
+  const [left, setLeft] = useState(x + OFFSET[0]);
+  const [top, setTop] = useState(y + OFFSET[1]);
+
+  useEffect(() => {
+    if (el.current) {
+      const elemBounds = 
+        el.current.firstChild.getBoundingClientRect();
+        
+      const mapBounds =
+        el.current.closest('.p6o-map-container')
+          .getBoundingClientRect();
+
+      const exceedsRight = elemBounds.right > mapBounds.right;
+      const exceedsBottom = elemBounds.bottom > mapBounds.bottom;
+
+      if (exceedsRight) 
+        setLeft(mapBounds.right - elemBounds.width - OFFSET[0]);
+
+      if (exceedsBottom)
+        setTop(mapBounds.bottom - elemBounds.height - OFFSET[1]);
+    }
+  }, [el.current?.getBoundingClientRect()]);
 
   return (
     <div 
+      ref={el}
       className="p6o-map-tooltip-container"
       style={{left, top}}>
 
