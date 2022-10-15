c86d7a3681a5bd3e5ff81c03cc593131c1073a24
Rainer Simon <rainer@rainersimon.io>
2022-10-15 16:20:27 +0200

    initial bounds support for MapLibre basemap

---
+++ src/map/components/Map/initialState.ts
@@ -0,0 +1,18 @@
+import { WebMercatorViewport } from '@deck.gl/core/typed';
+import { ViewState } from '../../types';
+
+export const getDefaultViewState = (initialBounds: [[number, number], [number, number]], el: HTMLDivElement) => {
+  const { offsetWidth, offsetHeight } = el;
+
+  const viewport = new WebMercatorViewport({
+    width: offsetWidth,
+    height: offsetHeight
+  });
+
+  return viewport.fitBounds(initialBounds, {});
+}
+
+export const isValidViewState = (viewState: ViewState) => {
+  const { longitude, latitude, zoom } = viewState;
+  return longitude && latitude && zoom;
+}
