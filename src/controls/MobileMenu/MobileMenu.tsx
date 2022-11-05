e8a624dddf152938006629d9eb25c94b83fb73f2
Rainer Simon <rainer@rainersimon.io>
2022-11-05 11:01:10 +0100

    Animated mobile menu entry

--- src/controls/MobileMenu/MobileMenu.tsx
+++ src/controls/MobileMenu/MobileMenu.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { FiMenu } from 'react-icons/fi';
+import { IoCloseOutline } from 'react-icons/io5';
 
 import './MobileMenu.css';
 
-export const MobileMenu = () => {
+type MobileMenuProps = {
+
+  items: React.ReactElement[]
+
+}
+
+export const MobileMenu = (props: MobileMenuProps) => {
+
+  const [open, setOpen] = useState(false);
 
   return (
-    <button 
-      className="p6o-controls-btn p6o-mobile-menu"
-      tabIndex={42}
-      aria-label="Open menu">
-      <FiMenu />
-    </button>
+    <>
+      <button 
+        className="p6o-controls-btn p6o-mobile-menu-btn"
+        tabIndex={42}
+        aria-label="Open menu"
+        onClick={() => setOpen(true)}>
+        <FiMenu />
+      </button>
+
+      <CSSTransition
+        in={open}
+        className="p6o-mobile-menu"
+        classNames="p6o-mobile-menu"
+        timeout={200}
+        unmountOnExit>
+
+        <div className="p6o-mobile-menu">
+          <button
+            className="p6o-mobile-menu-close"
+            onClick={() => setOpen(false)}>
+
+            <IoCloseOutline />
+
+          </button>
+        </div>
+
+      </CSSTransition>
+    </>
   )
 
 }
