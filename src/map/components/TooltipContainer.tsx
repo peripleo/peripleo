ec8da61f468cc4ab9f6309c59626bcc5f6601d39
Rainer Simon <rainer@rainersimon.io>
2022-11-12 15:55:19 +0100

    Refactoring: moved auto-position logic into a custom hook

--- src/map/components/TooltipContainer.tsx
+++ src/map/components/TooltipContainer.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-
-const OFFSET = [15, 15];
+import React, { useMemo, useRef } from 'react';
+import { useAutoPosition } from './useAutoPosition';
 
 type TooltipContainerProps = {
 
@@ -22,32 +21,10 @@ export const TooltipContainer = (props: TooltipContainerProps) => {
 
   const { x, y, node, feature, tooltip } = props;
 
-  const [ left, setLeft ] = useState(x + OFFSET[0]);
-  const [ top, setTop ] = useState(y + OFFSET[1]);
+  const { left, top } = useAutoPosition(el, x, y);
 
   const renderedTooltip = useMemo(() => 
     node ? tooltip({ node, feature }) : null, [ node, feature ]);
-
-  useEffect(() => {
-    if (el.current) {
-      const elemBounds = 
-        (el.current.firstChild as Element)?.getBoundingClientRect();
-        
-      const mapBounds =
-        el.current.closest('.p6o-map-container')?.getBoundingClientRect();
-
-      if (elemBounds && mapBounds) {
-        const exceedsRight = elemBounds.right > mapBounds.right;
-        const exceedsBottom = elemBounds.bottom > mapBounds.bottom;
-
-        if (exceedsRight) 
-          setLeft(mapBounds.right - elemBounds.width - OFFSET[0]);
-
-        if (exceedsBottom)
-          setTop(mapBounds.bottom - elemBounds.height - OFFSET[1]);
-      }
-    }
-  }, [el.current?.getBoundingClientRect()]);
 
   return renderedTooltip && (
     <div 
---
+++ src/map/components/useAutoPosition.ts
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+export const useAutoPosition = (
+  ref: React.RefObject<Element>, 
+  x: number, 
+  y: number, 
+  offset: [number, number] = [15, 15]
+) => {
+
+  const [ left, setLeft ] = useState(x + offset[0]);
+
+  const [ top, setTop ] = useState(y + offset[1]);
+
+  useEffect(() => {
+    if (ref.current) {
+      const elemBounds = 
+        (ref.current.firstChild as Element)?.getBoundingClientRect();
+        
+      const mapBounds =
+        ref.current.closest('.p6o-map-container')?.getBoundingClientRect();
+
+      if (elemBounds && mapBounds) {
+        const exceedsRight = elemBounds.right > mapBounds.right;
+        const exceedsBottom = elemBounds.bottom > mapBounds.bottom;
+
+        if (exceedsRight) 
+          setLeft(mapBounds.right - elemBounds.width - offset[0]);
+
+        if (exceedsBottom)
+          setTop(mapBounds.bottom - elemBounds.height - offset[1]);
+      }
+    }
+  }, [ ref.current?.getBoundingClientRect() ]);
+
+  return { top, left };
+
+}
