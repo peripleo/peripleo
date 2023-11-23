25739a1365dacd14881221f3a529d6178cba9067
Rainer Simon <rainer@rainersimon.io>
2023-11-23 12:48:30 +0100

    Basic search box style

--- packages/prototyping-atlas/src/components/SearchBox/SearchBox.tsx
+++ packages/prototyping-atlas/src/components/SearchBox/SearchBox.tsx
@@ -1,5 +1,6 @@
 import { FormEvent } from 'react';
 import { SearchBoxProps, useSearchBox } from 'react-instantsearch';
+import { Search, Settings2 } from 'lucide-react';
 
 export const SearchBox = (props: SearchBoxProps) => {
 
@@ -11,12 +12,22 @@ export const SearchBox = (props: SearchBoxProps) => {
   const onSubmit = (evt: FormEvent) => evt.preventDefault();
 
   return (
-    <form className="p6o-control" onSubmit={onSubmit}>
-      <input 
-        className="py-2 px-3.5 text-base rounded-full w-[300px] border"
-        spellCheck={false}
-        value={query} 
-        onChange={evt => refine(evt.target.value)} />
+    <form onSubmit={onSubmit} className="flex gap-2 items-center p-3 border-b">
+      <div className="relative flex-grow">
+        <Search className="absolute h-full w-4 ml-2 flex pb-0.5" />
+
+        <input 
+          className="w-full text-sm py-1.5 pr-3 pl-8 rounded-full bg-slate-300 placeholder-slate-
+            focus:outline-2 focus:outline-offset-2 focus:outline-teal-700"
+          spellCheck={false}
+          value={query} 
+          placeholder="Search"
+          onChange={evt => refine(evt.target.value)} />
+      </div>
+
+      <button className="p-2 rounded-full hover:bg-slate-300">
+        <Settings2 className="h-4 w-4" />
+      </button>
     </form>
   )
   
--- packages/prototyping-atlas/src/pages/Search/Search.tsx
+++ packages/prototyping-atlas/src/pages/Search/Search.tsx
@@ -3,7 +3,7 @@ import { SearchBox } from '../../components';
 export const Search = () => {
 
   return (
-    <aside className="absolute z-10 h-full bg-white/70 backdrop-blur shadow">
+    <aside className="absolute z-10 h-full w-[280px] bg-white/70 backdrop-blur shadow">
       <SearchBox />
     </aside>
   )
