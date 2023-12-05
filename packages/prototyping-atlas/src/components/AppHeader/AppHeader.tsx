096fa4c5f9248253d2953899a46e95d92c97b905
Rainer Simon <rainer@rainersimon.io>
2023-12-05 07:27:03 +0100

    Title branding

--- packages/prototyping-atlas/src/components/AppHeader/AppHeader.tsx
+++ packages/prototyping-atlas/src/components/AppHeader/AppHeader.tsx
@@ -1,3 +1,5 @@
+const { VITE_BRANDING_TITLE } = import.meta.env;
+
 export const AppHeader = (props: { className?: string }) => {
 
   const className = `${props.className || ''} w-full p-3 bg-black text-white`.trim();
@@ -5,7 +7,7 @@ export const AppHeader = (props: { className?: string }) => {
   return (
     <header className={className}>
       <h1 className="font-dm-display font-light text-3xl">
-        Atlas
+        {VITE_BRANDING_TITLE}
       </h1>
     </header>
   )
