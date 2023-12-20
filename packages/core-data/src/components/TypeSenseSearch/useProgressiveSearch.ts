4af3685ca732fc67a3a8153f0fd11b3d1d21a818
Rainer Simon <rainer@rainersimon.io>
2023-12-20 12:15:55 +0100

    Fixed regression bug that prevented initial auto-zoom

--- packages/core-data/src/components/TypeSenseSearch/useProgressiveSearch.ts
+++ packages/core-data/src/components/TypeSenseSearch/useProgressiveSearch.ts
@@ -31,12 +31,11 @@ export const useProgressiveSearch = () => {
 
   const map = useMap();
 
-  const hasStateChanged = (a: any, b: any) => {
-    // Ignore changes to page number!
-    if (a)
+  const hasStateChanged = (a: any, b: any, ignorePageNo?: boolean) => {
+    if (ignorePageNo && a)
       delete a.page;
   
-    if (b)
+    if (ignorePageNo && b)
       delete b.page;
 
     return !dequal(a, b);
@@ -45,12 +44,11 @@ export const useProgressiveSearch = () => {
   useEffect(() => {
     const { results } = infiniteHits;
 
-    const stateChanged = hasStateChanged(results._state, lastSearchState.current);
     const isFirstPage = results.page === 0;
     const isLastPage = results.page + 1 >= results.nbPages;
 
     // Add to cache and load next page
-    if (isFirstPage && stateChanged) {
+    if (isFirstPage && hasStateChanged(results._state, lastSearchState.current, true)) {
       setCachedHits(() => createCachedHits(results.hits as unknown as TypeSenseSearchResult[]));
     } else {
       setCachedHits(({ merge }) => merge(results.hits as unknown as TypeSenseSearchResult[]));
@@ -62,7 +60,7 @@ export const useProgressiveSearch = () => {
       const hasHits = cachedHits.hits.length > 0 && results.hits.length > 0;
       const isBoundsFilterActive = geoSearch.isRefinedWithMap();
 
-      if (map && hasHits && stateChanged && !isBoundsFilterActive) {      
+      if (map && hasHits && hasStateChanged(results._state, lastSearchState.current) && !isBoundsFilterActive) {      
         const features = {
           type: 'FeatureCollection',
           features: cachedHits.hits
