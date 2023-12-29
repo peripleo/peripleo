3b0720e9702db25c9fc7f71ba7213efede5e2021
Rainer Simon <rainer@rainersimon.io>
2023-12-29 10:02:39 +0100

    Some basic scaffolding for the media gallery dialog

--- packages/core-data/src/pages/SiteDetails/SiteDetails.tsx
+++ packages/core-data/src/pages/SiteDetails/SiteDetails.tsx
@@ -56,52 +56,54 @@ export const SiteDetails = () => {
   }, [related]);
 
   return (
-    <aside className="flex flex-col absolute z-10 h-full w-[280px] bg-white/80 backdrop-blur 
-      shadow overflow-y-auto">
-      <Link 
-        className="absolute top-2 right-2 p-1.5 rounded-full z-10 bg-slate-200/60 hover:bg-slate-200 focus:outline-2 focus:outline-offset-2 focus:outline-teal-700"
-        to="/">
-        <X className="h-4 w-4" />
-      </Link>
+    <>
+      <aside className="flex flex-col absolute z-10 h-full w-[280px] bg-white/80 backdrop-blur 
+        shadow overflow-y-auto">
+        <Link 
+          className="absolute top-2 right-2 p-1.5 rounded-full z-10 bg-slate-200/60 hover:bg-slate-200 focus:outline-2 focus:outline-offset-2 focus:outline-teal-700"
+          to="/">
+          <X className="h-4 w-4" />
+        </Link>
 
-      {site && (
-        <>
-          {headerImage && (
-            <div className="relative w-full h-[200px] flex-grow-0 flex-shrink-0 bg-muted/20 z-0">
-              <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-                <Image className="h-20 w-20 text-gray-400" strokeWidth={1} />
-              </div>
+        {site && (
+          <>
+            {headerImage && (
+              <div className="relative w-full h-[200px] flex-grow-0 flex-shrink-0 bg-muted/20 z-0">
+                <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+                  <Image className="h-20 w-20 text-gray-400" strokeWidth={1} />
+                </div>
 
-              <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-              <img
-                className="object-cover h-full w-full"
-                src={headerImage.url} 
-                alt={headerImage.title} />
+                <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+                <img
+                  className="object-cover h-full w-full"
+                  src={headerImage.url} 
+                  alt={headerImage.title} />
+                </div>
               </div>
+            )}
+
+            <div className="p-3">
+              <h1 className="pr-6 font-medium">
+                {site.properties.title}
+              </h1>
+
+              <ol className="text-sm mt-4 leading-6 overflow-hidden">
+                {userDefined.map(({ label, value }) => (
+                  <li key={label} className="mb-2">
+                    <div className="text-muted">{label}</div>
+                    <div className="font-medium overflow-hidden text-ellipsis">{value}</div>
+                  </li>
+                ))}
+              </ol>
             </div>
-          )}
 
-          <div className="p-3">
-            <h1 className="pr-6 font-medium">
-              {site.properties.title}
-            </h1>
-
-            <ol className="text-sm mt-4 leading-6 overflow-hidden">
-              {userDefined.map(({ label, value }) => (
-                <li key={label} className="mb-2">
-                  <div className="text-muted">{label}</div>
-                  <div className="font-medium overflow-hidden text-ellipsis">{value}</div>
-                </li>
-              ))}
-            </ol>
-          </div>
-
-          <RelatedItemsList 
-            recordId={recordId} 
-            items={related} />
-        </>
-      )}
-    </aside>
+            <RelatedItemsList 
+              recordId={recordId} 
+              items={related} />
+          </>
+        )}
+      </aside>
+    </>
   )
 
 }
