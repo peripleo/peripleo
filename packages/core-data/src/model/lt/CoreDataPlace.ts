ad3f18e880053c96754c5f8c3e808846fae2263c
Rainer Simon <rainer@rainersimon.io>
2024-01-12 11:21:45 +0100

    Related places are now shown again, and rendered directly from the annotation response

--- packages/core-data/src/model/lt/CoreDataPlace.ts
+++ packages/core-data/src/model/lt/CoreDataPlace.ts
@@ -1,11 +1,15 @@
-import { Annotation } from '@peripleo/peripleo';
+import { Annotation, FeatureGeometry } from '@peripleo/peripleo';
 import { CoreDataAnnotation } from './CoreDataAnnotation';
 
 export interface CoreDataPlace extends CoreDataAnnotation {
 
   type: 'Place';
 
+  geometry: FeatureGeometry;
+
 }
 
-export interface PlaceAnnotation extends Annotation<CoreDataPlace> { } 
+export interface PlaceAnnotation extends Annotation<CoreDataPlace> { 
+
+} 
 
--- packages/core-data/src/pages/SiteDetails/SiteDetailsLayer.tsx
+++ packages/core-data/src/pages/SiteDetails/SiteDetailsLayer.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { FeatureCollection } from '@peripleo/peripleo';
 import bbox from '@turf/bbox';
 import { MixedGeoJSONLayer, PulsingMarkerLayer, useMap } from '@peripleo/peripleo/maplibre';
 import { CoreDataPlace, CoreDataPlaceFeature, toFeature } from '../../model';
 import { POINT_STYLE, FILL_STYLE, STROKE_STYLE } from '../../layerStyles';
-import { useRuntimeConfig } from '../../CoreDataConfig';
 import { SiteDetailsTooltip } from './SiteDetailsTooltip';
 
 interface SiteDetailsLayerProps {
@@ -19,64 +18,49 @@ interface SiteDetailsLayerProps {
 
 export const SiteDetailsLayer = (props: SiteDetailsLayerProps) => {
 
-  const { place } = props;
-
-  const geometry: FeatureCollection = {
-    type: 'FeatureCollection',
-    features: [toFeature(place, place.properties.uuid)]
-  };
-
-  const { core_data } = useRuntimeConfig();
-
   const map = useMap();
 
-  const [related, setRelated] = useState<FeatureCollection>();
+  const place = {
+    type: 'FeatureCollection',
+    features: [toFeature(props.place, props.place.properties.uuid)]
+  } as FeatureCollection;
 
-  const [loaded, setLoaded] = useState(false);
+  const related = {
+    type: 'FeatureCollection', 
+    features: (props.related || []).map(p => ({
+      id: parseInt(p.record_id),
+      type: 'Feature',
+      properties: {
+        title: p.title,
+        record_id: p.record_id,
+      },
+      geometry: p.geometry
+    }))
+  } as FeatureCollection; 
 
   useEffect(() => {
-    if (loaded) {
-      const all = related
-        ? { type: 'FeatureCollection', features: [...geometry.features, ...related.features]}
-        : geometry;
+    const allFeatures = { 
+      type: 'FeatureCollection', 
+      features: [...place.features, ...related.features]
+    };
 
-      const [minX, minY, maxX, maxY] = bbox(all);
-      
-      map.fitBounds([[minX, minY], [maxX, maxY]], { 
-        padding: { top: 100, bottom: 100, left: 380, right: 120 },
-        maxZoom: 14
-      });
-    }
-  }, [loaded, related]);
+    const [minX, minY, maxX, maxY] = bbox(allFeatures);
 
-  useEffect(() => {
-    if (props.related) {
-      const urls = props.related.map(r =>
-        `${core_data.url}/core_data/public/places/${props.uuid}?project_ids=${core_data.project_ids.join(',')}`);
-
-      Promise.all(urls.map(url => fetch(url).then(res => res.json())))
-        .then(places => {
-          const features = places.map(p => toFeature(p, p.uuid));
-
-          setRelated({
-            type: 'FeatureCollection',
-            features
-          });
-
-          setLoaded(true);
-        });
-    }
-  }, [props.related?.map(r => r.id).join()]);
+    map.fitBounds([[minX, minY], [maxX, maxY]], { 
+      padding: { top: 100, bottom: 100, left: 380, right: 120 },
+      maxZoom: 14
+    });
+  }, [related]);
 
   return (
     <>
       <PulsingMarkerLayer 
         id="current" 
-        data={geometry} />
+        data={place} />
 
-      {related && (
+      {props.related && (
         <MixedGeoJSONLayer
-          id={`${place.record_id}-related`} 
+          id={`${props.place.record_id}-related`} 
           fillStyle={FILL_STYLE}
           strokeStyle={STROKE_STYLE}
           pointStyle={POINT_STYLE}
@@ -87,8 +71,8 @@ export const SiteDetailsLayer = (props: SiteDetailsLayerProps) => {
       )}
 
       <MixedGeoJSONLayer
-        id={place.record_id} 
-        data={geometry} 
+        id={props.place.record_id} 
+        data={place} 
         fillStyle={FILL_STYLE} 
         strokeStyle={STROKE_STYLE} 
         pointStyle={POINT_STYLE} 
