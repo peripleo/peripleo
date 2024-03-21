8c1625c03461de89cc929187b2fdb9af815267f4
Rainer Simon <rainer@rainersimon.io>
2024-03-21 16:07:26 +0100

    Skeleton example for pulsing marker

---
+++ packages/peripleo-maplibre/test/components/PulsingMarker/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Peripleo, Map } from '../../../src';
+
+import '@peripleo/maplibre/peripleo-maplibre.css';
+import '@peripleo/peripleo/default-theme';
+
+const App = () => {
+
+  return (
+    <Map
+    style="https://api.maptiler.com/maps/voyager/style.json?key=RFavxpVJ82EHyrN2kxsF">
+
+    </Map>
+  )
+
+}
+
+window.onload = function() {
+
+  ReactDOM.createRoot(document.getElementById('app')!).render(
+    <React.StrictMode>
+      <Peripleo>
+        <App />
+      </Peripleo>
+    </React.StrictMode>
+  )
+    
+}
