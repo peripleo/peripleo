fe4277bf0b7dc4d15af8b59d2e0522fa83a7303f
Rainer Simon <rainer@rainersimon.io>
2024-06-14 13:46:58 +0200

    More typing tweaks

--- packages/peripleo-maplibre/src/hooks/useHover.ts
+++ packages/peripleo-maplibre/src/hooks/useHover.ts
@@ -1,14 +1,20 @@
 import { 
   useHoverState as useGenericHoverState,
-  useHoverValue as useGenericHoverValue
+  useHoverValue as useGenericHoverValue,
+} from '@peripleo/peripleo';
+import type { 
+  Feature,
+  HoverState as GenericHoverState
 } from '@peripleo/peripleo';
 import { MapGeoJSONFeature, MapLayerMouseEvent, MapMouseEvent } from 'maplibre-gl';
 
+export type HoverState<T extends Feature> = GenericHoverState<T, MapGeoJSONFeature, MapMouseEvent>;
+
 // mapLibre-typed version of Peripleo's useHoverState
-export const useHoverState = <T>() => 
+export const useHoverState = <T extends Feature>() => 
   useGenericHoverState<T, MapGeoJSONFeature, MapMouseEvent>();
 
 
 // mapLibre-typed version of Peripleo's useHoverValue
-export const useHoverValue = <T>() =>
+export const useHoverValue = <T extends Feature>() =>
   useGenericHoverValue<T, MapGeoJSONFeature, MapLayerMouseEvent>();
--- packages/peripleo-maplibre/src/hooks/useSelection.ts
+++ packages/peripleo-maplibre/src/hooks/useSelection.ts
@@ -1,9 +1,14 @@
 import { 
-  Feature,
   useSelectionState as useGenericSelectionState,
   useSelectionValue as useGenericSelectionValue
 } from '@peripleo/peripleo';
+import type { 
+  Feature,
+  SelectionState as GenericSelectionState
+} from '@peripleo/peripleo';
 import { MapGeoJSONFeature, MapLayerMouseEvent, MapMouseEvent } from 'maplibre-gl';
+
+export type SelectionState<T extends Feature> = GenericSelectionState<T, MapGeoJSONFeature, MapMouseEvent>;
 
 // mapLibre-typed version of Peripleo's useHoverState
 export const useSelectionState = <T extends Feature>() => 
