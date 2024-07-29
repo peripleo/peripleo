5a73247162f1affc869995cbad748301a5ea738e
Rainer Simon <rainer@rainersimon.io>
2024-07-29 10:45:45 +0200

    Improvements to MyLocationControl

--- packages/peripleo-maplibre/src/components/MyLocationControl/MyLocationControl.tsx
+++ packages/peripleo-maplibre/src/components/MyLocationControl/MyLocationControl.tsx
@@ -7,6 +7,16 @@ interface MyLocationControlProps {
 
   children?: ReactNode;
 
+  enableHighAccuracy?: boolean;
+
+  onBusy?(busy: boolean): void;
+
+  onClick?(): void;
+
+  onError?(): void;
+
+  onSuccess?(): void;
+
 }
 
 export const MyLocationControl = (props: MyLocationControlProps) => {
@@ -15,16 +25,31 @@ export const MyLocationControl = (props: MyLocationControlProps) => {
 
   const onSuccess = (position: GeolocationPosition) => {
     const { latitude, longitude } = position.coords;
+
+    if (props.onSuccess) props.onSuccess();
+
+    if (props.onBusy) props.onBusy(false);
+
     map?.easeTo({ center: [ longitude, latitude ], zoom: 12 });
   }
 
-  const onError = (error: GeolocationPositionError) =>
+  const onError = (error: GeolocationPositionError) => {
     console.warn('Error fetching location: ' + error.message);
 
-  const onClick = () => 
+    if (props.onError) props.onError();
+
+    if (props.onBusy) props.onBusy(false);
+  }
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+
+    if (props.onBusy) props.onBusy(true);
+
     navigator.geolocation.getCurrentPosition(onSuccess, onError, {
-      enableHighAccuracy: true
+      enableHighAccuracy: props.enableHighAccuracy === undefined ? true : props.enableHighAccuracy
     });
+  }
 
   return (
     <button 
